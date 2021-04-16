b7a343a7cd81b18410f8cf40397defaf32f2e1c3
Fedor <fedor@dps.uibk.ac.at>
2021-04-16 19:42:25 +0200

    separated the api project

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -4,9 +4,6 @@ export const config = {
   "database": process.env.DB_NAME,
   "host": process.env.DB_HOST,
   'dialect': 'postgres',
-  'aws_region': "us-east-1",
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
